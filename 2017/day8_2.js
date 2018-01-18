d36f41ce7346c2dfaeb61b85e9b7d06b2c96066c
Jason Croft <jasoncroft30@gmail.com>
2018-01-17 23:38:05 -0500

    Day 8, Part 2 Solution

---
+++ 2017/day8_2.js
@@ -0,0 +1,31 @@
+const puzzle = ``; // Puzzle Input
+
+const plainTextInstructions = puzzle.split(/\n/);
+
+const registers = Array.from(new Set(plainTextInstructions.map((c) => c.split(' ')[0]))).reduce((p,c) => {
+    p[c] = 0;
+    return p;
+}, {});
+
+const instructions = plainTextInstructions.map((c) => {
+    const parts = c.split(' ');
+    return {
+        additive: Number(parts[2]),
+        check: parts[4],
+        conditional: [parts[5], parts[6]].join(' '),
+        multiplier: parts[1] === 'inc' ? 1 : -1,
+        name: parts[0],
+    }
+});
+
+let max = 0;
+instructions.forEach((c) => {
+    if (eval(`${registers[c.check]} ${c.conditional}`)) {
+        registers[c.name] += c.additive * c.multiplier;
+    }
+    if (registers[c.name] > max) {
+        max = registers[c.name];
+    }
+});
+
+console.log(max);
