c511b6eadc9828c6e16cd365840bef19815cf99a
Jason Croft <jasoncroft30@gmail.com>
2024-12-08 15:13:17 -0500

    Improved Day 6 Part 1

--- 2024/src/day6_1.ts
+++ 2024/src/day6_1.ts
@@ -9,10 +9,66 @@ const input = `....#.....
 #.........
 ......#...`;
 
+class Heading {
+    private _columnDirection: Direction.Left | Direction.Right = Direction.Right;
+    private _lineDirection: Direction.Down | Direction.Up = Direction.Up;
+
+    public constructor(
+        private _columnHeading: -1 | 0 | 1,
+        private _lineHeading: -1 | 0 | 1,
+    ) {
+        
+    }
+
+    get columnHeading() {
+        return this._columnHeading;
+    }
+
+    get delta() {
+        return [this._lineHeading, this._columnHeading];
+    }
+
+    get lineHeading() {
+        return this._lineHeading;
+    }
+
+    turn90() {
+        switch (this._columnHeading) {
+            case 0:
+                this._columnHeading += this._columnDirection === Direction.Left ? -1 : 1;
+                break;
+            case -1:
+            case 1:
+                this._columnDirection = this._columnDirection === Direction.Left ? Direction.Right : Direction.Left;
+                this._columnHeading = 0;
+        }
+
+        switch (this._lineHeading) {
+            case 0:
+                this._lineHeading += this._lineDirection === Direction.Up ? -1 : 1;
+                break;
+            case -1:
+            case 1:
+                this._lineDirection = this._lineDirection === Direction.Up ? Direction.Down : Direction.Up;
+                this._lineHeading = 0;
+        }
+    }
+
+    turn180() {
+        this.turn90();
+        this.turn90();
+    }
+
+    turn270() {
+        this.turn180();
+        this.turn90();
+    }
+}
+
 class MapCrawler {
-    private _currentDirection: Direction = Direction.Up;
+    private _currentColumn: number;
     private _currentLine: number;
-    private _currentCol: number;
+    private _heading: Heading;
     private _mapString: string;
     private _parsedMap: Array<string[]>;
     private _spacesVisited: string[] = [];
@@ -22,20 +78,22 @@ class MapCrawler {
         this._parsedMap = init.split(`\n`).map((line, ix) => {
             if (line.indexOf('^') > -1) {
                 this._currentLine = ix;
-                this._currentCol = line.indexOf('^');
+                this._currentColumn = line.indexOf('^');
                 this._spacesVisited.push(this.currentPosition.join(','));
             }
 
             return line.split('')
         });
+
+        this._heading = new Heading(0, -1);
     }
 
     get currentPosition() {
-        return [this._currentLine, this._currentCol];
+        return [this._currentLine, this._currentColumn];
     }
 
     get guardInArea() {
-        return this._currentLine >= 0 && this._currentLine < this._parsedMap.length - 1 && this._currentCol >= 0 && this._currentCol < this._parsedMap[0].length - 1;
+        return this._currentLine >= 0 && this._currentLine < this._parsedMap.length - 1 && this._currentColumn >= 0 && this._currentColumn < this._parsedMap[0].length - 1;
     }
 
     private isSpaceOpen(checkLine: number, checkCol: number) {
@@ -43,22 +101,18 @@ class MapCrawler {
     }
 
     public move() {
-        const [lineDir, colDir] = this.parseDirection();
-        if (this.isSpaceOpen(this._currentLine + lineDir, this._currentCol + colDir)) {
+        if (this.isSpaceOpen(this._currentLine + this._heading.lineHeading, this._currentColumn + this._heading.columnHeading)) {
             if (this._spacesVisited.indexOf(this.currentPosition.join(',')) === -1) {
                 this._spacesVisited.push(this.currentPosition.join(','));
             }
-            this._currentCol += colDir;
-            this._currentLine += lineDir;
+
+            this._currentColumn += this._heading.columnHeading;
+            this._currentLine += this._heading.lineHeading;
             return;
         }
 
-        this._currentDirection = turn90(this._currentDirection);
+        this._heading.turn90();
         this.move();
-    }
-
-    private parseDirection() {
-        return this._currentDirection.split(',').map(i => parseInt(i, 10));
     }
 
     get spacesVisited() {
@@ -67,25 +121,11 @@ class MapCrawler {
 }
 
 enum Direction {
-    Down = '1,0',
-    Left = '0,-1',
-    Up = '-1,0',
-    Right = '0,1',
+    Down,
+    Left,
+    Up,
+    Right,
 }
-
-const turn90 = (currDir: Direction): Direction => {
-    switch (currDir) {
-        case Direction.Down:
-            return Direction.Left;
-        case Direction.Left:
-            return Direction.Up;
-        case Direction.Up:
-            return Direction.Right;
-        case Direction.Right:
-        default:
-            return Direction.Down;
-    }
-};
 
 const crawler = new MapCrawler(input);
 while (crawler.guardInArea) {
