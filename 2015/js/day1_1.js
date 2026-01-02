dbc16858ade1cb6fc18abe3a9a3ca431c3541ba8
Jason Croft <jasoncroft30@gmail.com>
2026-01-01 21:15:23 -0500

    Remove args parser

--- 2015/js/day1_1.js
+++ 2015/js/day1_1.js
@@ -1,13 +1,5 @@
 const input = ``;
 
-const args = process.argv.slice(2).reduce((p,c) => {
-    const [key, value] = c.split('=');
-    return {
-        ...p,
-        [key.replace(/--/, '')]: value || true,
-    }
-}, {});
-
 const solve = () => {
     const downs = input.match(/\)/g);
     const ups = input.match(/\(/g);
