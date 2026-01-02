945bf1a9f9148fcf841c69576fa4bb8b8c171117
Jason Croft <jasoncroft30@gmail.com>
2026-01-02 12:36:09 -0500

    Day 5 Part 2 Solution

---
+++ 2015/js/day5_2.js
@@ -0,0 +1,32 @@
+const input = ``;
+
+const args = process.argv.slice(2).reduce((p,c) => {
+    const [key, value] = c.split('=');
+
+    return {
+        ...p,
+        [key.replace(/--/, '')]: value || true,
+    }
+}, {});
+
+const solve = () => {
+    const strings = input.split(/\n/);
+    const nice = strings.reduce((p, str) => {
+        if (
+            (/(.).\1/g.test(str))
+            && (/(..).*\1/.test(str))
+        ) {
+            if (args.verbose) {
+                console.log(str);
+            }
+            
+            return p + 1;
+        }
+
+        return p;
+    }, 0);
+
+    console.log(nice);
+};
+
+solve();
